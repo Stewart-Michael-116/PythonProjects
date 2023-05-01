42b6324a452828910a60de939b75ae797919a0e3
Michael Stewart <stewart.michael116@gmail.com>
2023-04-30 21:13:18 -0600

    Add Milestone 2 and bascically 3

--- Portfolio/PortfolioProject.py
+++ Portfolio/PortfolioProject.py
@@ -1,10 +1,12 @@
 class ItemToPurchase:
-    
+    item_name = ''
+    item_price = 0.0
+    item_quantity = 0
+    total_price = 0.0
+    item_description = ""
+
     def __init__(self):
-        item_name = 'none'
-        item_price = 0.0
-        item_quantity = 0
-        total_price = 0.0
+        pass
 
     def print_item_cost(self):
         self.total_price = self.item_quantity * self.item_price
@@ -12,14 +14,17 @@ class ItemToPurchase:
 
 class ShoppingCart:
 
+    customer_name = None
+    current_date = "January 1, 2020"
+    cart_items = []
+
     def __init__(self, customer_name, current_date):
-        customer_name = None
-        current_date = "January 1, 2020"
-        cart_items = []
+        self.customer_name = customer_name
+        self.current_date = current_date
         
 
     def add_item(self, item: ItemToPurchase):
-        self.append(item)
+        self.cart_items.append(item)
 
     def remove_item(self, item: str):
         for x in self.cart_items:
@@ -29,22 +34,22 @@ class ShoppingCart:
 
     # Needs to be finished
     def modify_item(self, item: ItemToPurchase):
-        for x in self.cart_items:
-            if(x.item_name == item.name):
-                if(item.item_name != 'none'):
-                    pass
+
+        for i in range(len(self.cart_items)):
+            if(self.cart_items[i].item_name == item.item_name):
+                #if(item.item_name != 'none'):
+                #    x
                 if(item.item_price != 0.0):
-                    pass
+                    self.cart_items[i].item_price = item.item_price
                 if(item.item_quantity != 0):
-                    pass
-                if(item.total_price != 0.0):
-                    pass
+                    self.cart_items[i].item_quantity = item.item_quantity
+                if(item.item_description != ""):
+                    self.cart_items[i].item_description = item.item_description
+                self.cart_items[i].total_price = self.cart_items[i].item_quantity * self.cart_items[i].item_price
+                print('\nItem Modified\n')
                 return
             
-        print('error message')
-
-        
-
+        print('Item not found in cart. Nothing modified.')
 
 
     def get_num_items_in_cart(self):
@@ -54,46 +59,135 @@ class ShoppingCart:
         total = 0
 
         for x in self.cart_items:
-            total += x.item_price
+            total += x.total_price
 
         return total
             
 
     def print_total(self):
-        pass
+        
+        print("\n{}'s Shopping Cart - {}".format(self.customer_name, self.current_date))
+        print("Number of Items: {}".format(self.get_num_items_in_cart()))
+
+        for x in self.cart_items:
+            print("{} {} @ ${} = ${}".format(x.item_name, x.item_quantity, x.item_price, x.total_price))
+
+        print("Total: ${}".format(self.get_cost_of_cart()))
 
     def print_descriptions(self):
-        pass
+        print("\n{}'s Shopping Cart - {}".format(self.customer_name, self.current_date))
+        print("Item Descriptions")
+
+        for x in self.cart_items:
+            print("{}: {}".format(x.item_name, x.item_description))
+
+    def print_menu(self):
+        print("\nMENU")
+        print("a - Add item to cart")
+        print("r - Remove item from cart")
+        print("c - Change item quantity")
+        print("i - Output items' descriptions")
+        print("o - Output shopping cart")
+        print("q - Quit\n")
 
 
 
 # Main Code
+#Milestone 2 Code
+#-----------------------------------------------------------------------------------------------------------------------------------------------
 
 # Definitions
-
-item1 = ItemToPurchase()
-
-item2 = ItemToPurchase()
-
-def print_menu(cart: ShoppingCart):
-    pass
+name = "Michael Stewart"
+date = "April 30, 2023"
+shopping_cart = ShoppingCart(name, date)
 
 
 
+while(1):
+    shopping_cart.print_menu()
+    customer_request = input("Please Enter a Menu Option:")
 
-# Prompt User for two items
+    if(customer_request == 'a'):
+        new_item = ItemToPurchase()
+        new_item.item_name = input("Please put the item's name:")
+        new_item.item_price = float(input("Please put the item's price:"))
+        new_item.item_quantity = int(input("Please put the item's quantity:"))
+        new_item.item_description = input("Please put the item's description:")
+        new_item.total_price = new_item.item_price * new_item.item_quantity
 
-item1.item_name = input("\nPlease enter the first item's name:\n")
-item1.item_price = float(input("\nPlease enter the first item's price:\n"))
-item1.item_quantity = int(input("\nPlease enter the first item's quantity:\n"))
+        shopping_cart.add_item(new_item)
 
-item2.item_name = input("\nPlease enter the second item's name:\n")
-item2.item_price = float(input("\nPlease enter the second item's price:\n"))
-item2.item_quantity = int(input("\nPlease enter the second item's quantity:\n"))
+        print("Thank you, item added\n")
 
-# Results
+    elif(customer_request == 'r'):
+        item_removing = input("Please enter the name of the item to be removed:")
+        shopping_cart.remove_item(item_removing)
 
-item1.print_item_cost()
-item2.print_item_cost()
+    elif(customer_request == 'c'):
 
-print('Total: ${}'.format((item1.total_price+item2.total_price)))
+        new_item = ItemToPurchase()
+        new_item.item_name = input("Please put the item's name you'd like to modify:")
+        new_item.item_price = float(input("Please put the item's price new price (press enter if no new price):"))
+
+        quantity = input("Please put the item's new quantity (press enter if no new quantity):")
+        if quantity == "":
+            new_item.item_quantity = 0
+        else:
+            new_item.item_quantity = int(quantity)
+        
+        new_item.item_description = input("Please put the item's new description (press enter if no new description):")
+
+        shopping_cart.modify_item(new_item)
+        
+    elif(customer_request == 'i'):
+        shopping_cart.print_descriptions()
+
+    elif(customer_request == 'o'):
+        shopping_cart.print_total()
+    
+    elif(customer_request == 'q'):
+        break
+    else:
+        print("Error: Incorrect Input")
+        continue
+        
+
+print("\nSee you later!\n")
+
+
+
+
+
+
+
+# Milestone 1 Code
+#-----------------------------------------------------------------------------------------------------------------------------------------------
+
+# # Definitions
+
+# item1 = ItemToPurchase()
+
+# item2 = ItemToPurchase()
+
+# def print_menu(cart: ShoppingCart):
+#     pass
+
+
+
+
+# # Prompt User for two items
+
+# item1.item_name = input("\nPlease enter the first item's name:\n")
+# item1.item_price = float(input("\nPlease enter the first item's price:\n"))
+# item1.item_quantity = int(input("\nPlease enter the first item's quantity:\n"))
+
+# item2.item_name = input("\nPlease enter the second item's name:\n")
+# item2.item_price = float(input("\nPlease enter the second item's price:\n"))
+# item2.item_quantity = int(input("\nPlease enter the second item's quantity:\n"))
+
+# # Results
+
+# item1.print_item_cost()
+# item2.print_item_cost()
+
+# print('Total: ${}'.format((item1.total_price+item2.total_price)))
